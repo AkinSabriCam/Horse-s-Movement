19c6665c444e4f0c2db57f75617f0af15bc6fddb
akın çam <sabricam2016@hotmail.com>
2018-05-07 23:20:18 +0300

    Gonderilenler

---
+++ Odev2/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace Odev2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OyunPanel = new System.Windows.Forms.DataGridView();
+            this.btnbasla = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnYeniden = new System.Windows.Forms.Button();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.OyunPanel)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // OyunPanel
+            // 
+            this.OyunPanel.AllowUserToAddRows = false;
+            this.OyunPanel.AllowUserToDeleteRows = false;
+            this.OyunPanel.AllowUserToResizeColumns = false;
+            this.OyunPanel.AllowUserToResizeRows = false;
+            this.OyunPanel.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.OyunPanel.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.OyunPanel.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.OyunPanel.Location = new System.Drawing.Point(142, 92);
+            this.OyunPanel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.OyunPanel.MultiSelect = false;
+            this.OyunPanel.Name = "OyunPanel";
+            this.OyunPanel.RowTemplate.Height = 24;
+            this.OyunPanel.Size = new System.Drawing.Size(33, 21);
+            this.OyunPanel.TabIndex = 0;
+            this.OyunPanel.Visible = false;
+            this.OyunPanel.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.OyunPanel_CellClick);
+            this.OyunPanel.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.OyunPanel_CellContentClick);
+            // 
+            // btnbasla
+            // 
+            this.btnbasla.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnbasla.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnbasla.Location = new System.Drawing.Point(12, 31);
+            this.btnbasla.Name = "btnbasla";
+            this.btnbasla.Size = new System.Drawing.Size(95, 38);
+            this.btnbasla.TabIndex = 1;
+            this.btnbasla.Text = "BAŞLA";
+            this.btnbasla.UseVisualStyleBackColor = false;
+            this.btnbasla.Click += new System.EventHandler(this.btnbasla_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 156);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(51, 21);
+            this.radioButton1.TabIndex = 2;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "7x7";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(12, 183);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(51, 21);
+            this.radioButton2.TabIndex = 3;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "8x8";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(12, 210);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(51, 21);
+            this.radioButton3.TabIndex = 4;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "9x9";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(196, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Skor:";
+            // 
+            // btnYeniden
+            // 
+            this.btnYeniden.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnYeniden.Enabled = false;
+            this.btnYeniden.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnYeniden.ForeColor = System.Drawing.Color.Black;
+            this.btnYeniden.Location = new System.Drawing.Point(504, 31);
+            this.btnYeniden.Name = "btnYeniden";
+            this.btnYeniden.Size = new System.Drawing.Size(176, 38);
+            this.btnYeniden.TabIndex = 6;
+            this.btnYeniden.Text = "YENİDEN BAŞLA";
+            this.btnYeniden.UseVisualStyleBackColor = false;
+            this.btnYeniden.Visible = false;
+            this.btnYeniden.Click += new System.EventHandler(this.btnYeniden_Click);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(13, 129);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(51, 21);
+            this.radioButton4.TabIndex = 7;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "6x6";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(13, 102);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(51, 21);
+            this.radioButton5.TabIndex = 8;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "5x5";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.CheckedChanged += new System.EventHandler(this.radioButton5_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(872, 612);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.btnYeniden);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.btnbasla);
+            this.Controls.Add(this.OyunPanel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.OyunPanel)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView OyunPanel;
+        private System.Windows.Forms.Button btnbasla;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnYeniden;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+    }
+}
+
---
+++ Odev2/Form1.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Odev2
+{
+    public partial class Form1 : Form
+    {
+        int X, Y, A, B, deger = 0, Puan = 1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void Temizle()
+        {
+            for (int i = 0; i < X; i++)
+            {
+                for (int j = 0; j < Y; j++)
+                {
+                    OyunPanel.Rows[j].Cells[i].Style.BackColor = Color.White;
+                }
+            }
+        }
+        public bool SayiKontrol(int x, int y)
+        {  // karelerde sayı olup olmadığının kontrolü
+            string s;
+            int n;
+            bool Sayi = false;
+            try
+            {
+                s = OyunPanel.Rows[y].Cells[x].Value.ToString();
+                if (!string.IsNullOrEmpty(s))
+                   Sayi = int.TryParse(s, out n);
+            }
+            catch
+            {
+                s = null;
+                Sayi = true;
+            }
+
+            return Sayi;
+        }
+        public void  Islem(int x, int y)
+        { 
+           //L şeklinde karelerin belirlenme işlemi yapılmakda  
+            int[] skorKontrol = new int[8];
+            int sayac = 0;
+
+            
+            Temizle();
+            if (y + 1 <= Y && x - 2 >= 0)
+            {
+                if (!SayiKontrol(x - 2, y + 1))
+                {
+                    OyunPanel.Rows[y + 1].Cells[x - 2].Style.BackColor = Color.LightPink;
+                    skorKontrol[0] = 1;
+                }
+            }
+            if (y + 1 <= Y && x + 2 <= X)
+            {
+                if (!SayiKontrol(x + 2, y + 1))
+                {
+                    OyunPanel.Rows[y + 1].Cells[x + 2].Style.BackColor = Color.LightPink;
+                    skorKontrol[1] = 1;
+                }
+            }
+            if (y - 1 >= 0 && x + 2 <= X)
+            {
+                if (!SayiKontrol(x + 2, y - 1))
+                {
+                    OyunPanel.Rows[y - 1].Cells[x + 2].Style.BackColor = Color.LightPink;
+                    skorKontrol[2] = 1;
+                }
+            }
+            if (y - 1 >= 0 && x - 2 >= 0)
+            {
+                if (!SayiKontrol(x - 2, y - 1))
+                {
+                    OyunPanel.Rows[y - 1].Cells[x - 2].Style.BackColor = Color.LightPink;
+                    skorKontrol[3] = 1;
+                }
+            }
+            if (y - 2 >= 0 && x + 1 <= X)
+            {
+                if (!SayiKontrol(x + 1, y - 2))
+                {
+                    OyunPanel.Rows[y - 2].Cells[x + 1].Style.BackColor = Color.LightPink;
+                    skorKontrol[4] = 1;
+                }
+            }
+            if (y - 2 >= 0 && x - 1 >= 0)
+            {
+                if (!SayiKontrol(x - 1, y - 2))
+                {
+                    OyunPanel.Rows[y - 2].Cells[x - 1].Style.BackColor = Color.LightPink;
+                    skorKontrol[5] = 1;
+                }
+            }
+            if (y + 2 <= Y && x - 1 >= 0)
+            {
+                if (!SayiKontrol(x - 1, y + 2))
+                {
+                    OyunPanel.Rows[y + 2].Cells[x - 1].Style.BackColor = Color.LightPink;
+                    skorKontrol[6] = 1;
+                }
+            }
+            if (y + 2 <= Y && x + 1 <= X)
+            {
+                if (!SayiKontrol(x + 1, y + 2))
+                {
+                    OyunPanel.Rows[y + 2].Cells[x + 1].Style.BackColor = Color.LightPink;
+                    skorKontrol[7] = 1;
+                }
+            }
+
+            //dizi elemanlarının hepsi sıfır ise oyun biter skoru ekrana yazdır...
+            for (int i = 0; i < 8; i++)
+            {
+                if (skorKontrol[i] == 0)
+                    sayac++;
+            }
+            if (sayac >= 8)
+            {
+                label1.Text +=(Puan - 1).ToString();
+                if (Puan - 1 == (X * Y))
+                 MessageBox.Show("Tebrikler Kazandınız!!");
+
+                MessageBox.Show("Oyun Bitti..");
+                Application.Restart();
+
+
+            }
+        }
+
+
+
+
+        private void OyunPanel_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnYeniden_Click(object sender, EventArgs e)
+        {
+
+           
+            MessageBox.Show("Yeni Oyun Yükleniyor..");
+            Application.Restart();
+
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            X = 6;
+            Y = 6;
+        }
+
+        private void radioButton5_CheckedChanged(object sender, EventArgs e)
+        {
+            X = 5;
+            Y = 5;
+        }
+
+        private void OyunPanel_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            A = OyunPanel.CurrentCellAddress.X;
+            B = OyunPanel.CurrentCellAddress.Y;
+            if (deger == 0) 
+            {
+                OyunPanel.Rows[B].Cells[A].Value = Puan;
+                Puan++;
+                Islem(A, B);
+                deger++;
+            }
+            if (OyunPanel.Rows[B].Cells[A].Style.BackColor == Color.LightPink && !SayiKontrol(A, B))
+            {
+                OyunPanel.Rows[B].Cells[A].Value = Puan;
+                Puan++;
+                Islem(A, B);
+            }
+
+        }
+
+
+
+        private void btnbasla_Click(object sender, EventArgs e)
+        {
+            DataTable panel = new DataTable();
+            OyunPanel.Visible = true;
+
+            if (X == 0)
+            {
+                MessageBox.Show("Oyun Boyutunu Belirleyiniz");
+                Application.Restart();
+            }
+            else
+            {
+                for (int i = 0; i < X; i++)
+                    panel.Columns.Add();
+                for (int j = 0; j < Y; j++)
+                    panel.Rows.Add();
+
+                OyunPanel.DataSource = panel;
+                foreach (DataGridViewRow row in OyunPanel.Rows)
+                {
+                    row.Height = 50;
+                }
+                foreach (DataGridViewColumn col in OyunPanel.Columns)
+                {
+                    col.Width = 50;
+                    OyunPanel.ColumnHeadersVisible = false;
+                    OyunPanel.RowHeadersVisible = false;
+                    OyunPanel.DefaultCellStyle.SelectionBackColor = OyunPanel.DefaultCellStyle.BackColor;
+                }
+                OyunPanel.ColumnHeadersVisible = false;
+                OyunPanel.RowHeadersVisible = false;
+                OyunPanel.AutoSize = true;
+
+                radioButton1.Visible = false;
+                radioButton2.Visible = false;
+                radioButton3.Visible = false;
+                radioButton4.Visible = false;
+                radioButton5.Visible = false;
+                btnbasla.Visible= false;
+                btnYeniden.Visible = true;
+                btnYeniden.Enabled = true;
+                if (X > 8 && Y > 8)
+                    this.WindowState = FormWindowState.Maximized;
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            X = 7;
+            Y = 7;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            X = 8;
+            Y = 8;
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            X = 9;
+            Y = 9;
+        }
+    }
+}
